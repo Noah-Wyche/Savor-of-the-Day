e325bba9c3e8fbccc393c6a03d6d985e565ab160
Amanda Aina <amandaade92@yahoo.com>
2023-11-02 21:10:50 -0400

    updated java

--- assets/pages/previous-recipes/previous.js
+++ assets/pages/previous-recipes/previous.js
@@ -1,52 +1,48 @@
-const apiKey = '91999b4d14f245a8862f6a2e80bfe912';
-const recipeID = '4632';
-const endpoint = `https://api.spoonacular.com/recipes/${recipeID}/card?apiKey=${apiKey}`;
+// Check if local storage contains previous recipe views
+var previousViews = localStorage.getItem('recipeViews');
+if (previousViews) {
+  previousViews = JSON.parse(previousViews);
+} else {
+  previousViews = [];
+}
 
-// Check if the 'visitedRecipes' data exists in localStorage
-const visitedRecipes = JSON.parse(localStorage.getItem('visitedRecipes')) || [];
+// Function to fetch and display recipe details from Spoonacular API
+function displayRecipe(recipeId) {
+  // Make a request to the Spoonacular API to fetch recipe details
+  fetch(`https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=91999b4d14f245a8862f6a2e80bfe912`)
+    .then(response => response.json())
+    .then(data => {
+      // Display recipe details on the page
+      var recipeTitle = data.title;
+      var recipeSummary = data.summary;
 
-// Function to fetch recipe information for each visited recipe
-function fetchVisitedRecipes() {
-  visitedRecipes.forEach((recipe) => {
-    const recipeID = recipe.id;
-    const recipeEndpoint = `https://api.spoonacular.com/recipes/${recipeID}/information?apiKey=${apiKey}`;
+      // Update the HTML with the recipe details
+      var recipeDetails = document.getElementById('recipe-details');
+      recipeDetails.innerHTML = `
+        <h2>${recipeTitle}</h2>
+        <p>${recipeSummary}</p>
+      `;
 
-    fetch(recipeEndpoint)
-      .then((response) => response.json())
-      .then((data) => {
-        // Handle the data here
-        console.log(data);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
+      // Store the recipe ID in local storage as a viewed recipe
+      previousViews.push(recipeId);
+      localStorage.setItem('recipeViews', JSON.stringify(previousViews));
+    })
+    .catch(error => {
+      console.error('Error fetching recipe details:', error);
+    });
+}
+
+// Display previously viewed recipes
+if (previousViews.length > 0) {
+  var recipeList = document.getElementById('recipe-list');
+  recipeList.innerHTML = '<h3>Previously Viewed Recipes:</h3>';
+
+  previousViews.forEach(recipeId => {
+    displayRecipe(recipeId);
   });
 }
 
-// After fetching the recipe information
-fetch(endpoint)
-  .then((response) => response.json())
-  .then((data) => {
-    // Check if the 'id' property exists in the response data
-    if ('id' in data) {
-      // Push the recipe ID to the visitedRecipes array
-      visitedRecipes.push({ id: data.id });
 
-      // Update local storage with the updated visitedRecipes
-      localStorage.setItem('visitedRecipes', JSON.stringify(visitedRecipes));
-
-      // Display the updated visited recipes
-      displayVisitedRecipes();
-    } else {
-      console.error('Recipe ID not found in response data.');
-    }
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
-
-// Call the function to fetch information for visited recipes
-fetchVisitedRecipes();
 
 
 
